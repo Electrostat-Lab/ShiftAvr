adb236f3395add35b85e6b5b43e87a32db4b2aef
pavl_g <scrappers.tm@gmail.com>
2023-04-11 13:55:17 +0200

    shiftavr-examples: implemented complete examples

--- shiftavr-examples/src/hello_adc.c
+++ shiftavr-examples/src/hello_adc.c
@@ -1,15 +1,66 @@
+#define F_CPU 16000000UL
 #include <adc/adc.h>
+#include <uart/uart.h>
+#include <util/delay.h>
 
-void adc_on_received(const uint16_t READ) {
-    // do stuff
-    // restart the adc conversion
-    adc_start_conversion(ADC_MUX0);
-}
+void adc_on_received(const uint16_t);
+void adc_on_protocol_started();
+void on_data_received(const uint8_t);
+void on_data_transmitted(const uint8_t);
 
-int main() {
+static inline void setup_adc() {
     adc_start_protocol();
     adc_enable_isr();
     adc_start_conversion(ADC_MUX0);
+}
+
+static inline void setup_uart() {
+    uart_start_protocol(BAUD_RATE_57600);
+}
+
+void adc_on_received(const uint16_t READ) {
+    // do stuff
+    uart_println(READ, 10);
+    // restart the adc conversion
+    setup_adc();
+}
+
+void adc_on_protocol_started() {
+}
+
+void on_data_received(const uint8_t datum) {
+}
+
+void on_data_transmitted(const uint8_t datum) {
+}
+
+/**
+ * @note volatile: marks this object as an interrupt-safe adjustable. 
+ */
+volatile adc_callbacks _adc_callbacks = {
+    adc_on_received,
+    NULL,
+    NULL,
+    NULL,
+};
+
+/**
+ * @note volatile: marks this object as an interrupt-safe adjustable. 
+ */
+volatile uart_callbacks _uart_callbacks = {
+    NULL, 
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+
+int main() {
+    adc_assign_callbacks(&_adc_callbacks);
+    uart_assign_callbacks(&_uart_callbacks);
+    setup_uart();
+    setup_adc();
+    
     while (1); /* Running forever! */
 	return 0;
 }
--- shiftavr-examples/src/hello_uart.c
+++ shiftavr-examples/src/hello_uart.c
@@ -1,14 +1,48 @@
+#define F_CPU 16000000UL
 #include <uart/uart.h>
 
-void onProtocolStarted() {
-
+void on_protocol_started() {
 }
 
-void onDataReceiveCompleted(const uint8_t s)  {
+void on_protocol_stopped() {
 }
+
+void on_data_received(const uint8_t datum) {
+    uart_cprint(datum);
+    uart_start_transmitter_isr();
+    uart_start_udre_isr();
+}
+
+void on_data_transmitted(const uint8_t datum) {
+    uart_stop_transmitter_isr();
+    uart_start_udre_isr();
+}
+
+void on_databuffer_cleared() {
+    uart_stop_udre_isr();
+}
+
+/**
+ * @note volatile: marks this object as an interrupt-safe adjustable. 
+ */
+volatile uart_callbacks _uart_callbacks = {
+    on_protocol_started,
+    on_protocol_stopped,
+    on_data_received,
+    on_data_transmitted,
+    on_databuffer_cleared
+};
 
 int main() {
-    startProtocol(2230);
+    /* assigns api callbacks */
+    uart_assign_callbacks(&_uart_callbacks);
+    /* start the protocol first to enable TX and RX and set BAUD! */
+    uart_start_protocol(BAUD_RATE_57600);
+    /* starting the ISR should be after starting the protocol! i.e, after enabling RX and TX! */
+    uart_start_udre_isr();
+    uart_start_receiver_isr();
+    uart_start_transmitter_isr();
+
     while (1); /* Running forever! */
-	return 0;
+    return 0;
 }
